0739bcbf4881df6d51d74f18a991f12482c93f70
Eazey <eazey@foxmail.com>
2017-04-17 23:54:08 +0800

    Update UI
    Set calculator UI.

---
+++ Assets/Scene/UIManager.cs
@@ -0,0 +1,33 @@
+﻿/*- - - - - - - - - - - - - - - - - - - - - - - - - -*/
+/*	  Script Editor: Eazey丶亦泽                      
+/*	  Blog   Adress: http://blog.csdn.net/eazey_wj     
+/*	  GitHub Adress: https://github.com/Eazey 		 
+/*- - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+/*	 Either none appetency, or determined to win.    */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Script Overview: 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	void Awake () 
+	{
+		
+	}
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		
+	}
+}
